0339f3e094d7df89d4d6ddf81b4c11c69b2c557b
vladyslavhorbachenko <vladgorbachenko1234@gmail.com>
2020-05-24 20:56:51 -0700

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+var PORT = process.env.PORT || 5000
 const express = require('express');
 const server = require('./server')
 
 
-
-server.listen(5000, () => console.log('Server is running on port 5000'));
+server.listen(PORT, () => console.log('Server is running on port 5000'));
