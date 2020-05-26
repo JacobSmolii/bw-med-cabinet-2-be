286b6c861ce89421c4cfc10a3874043839d1c56e
vladyslavhorbachenko <vladgorbachenko1234@gmail.com>
2020-05-25 17:33:04 -0700

    change BD

--- knexfile.js
+++ knexfile.js
@@ -23,10 +23,10 @@ production: {
   client: "pg",
       connection: process.env.DATABASE_URL,
       migrations: {
-    directory: "./data/migrations",
+    directory: "./database/migrations",
   },
   seeds: {
-    directory: "./data/seeds",
+    directory: "./database/seeds",
   },
 },
 
